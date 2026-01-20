1961633d637a631f5dd4740ce5eb1e7e36917a43
VanTai0302 <vanminhthanhtai121216@gmail.com>
2026-01-20 08:56:50 +0700

    Update

--- Demo4/src/App.vue
+++ Demo4/src/App.vue
@@ -1,4 +1,4 @@
-<!-- <script setup>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -11,15 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-</template> -->
-
-<script setup>
-import Bai3 from './components/Bai3.vue';
-</script>
-
-<template>
-<Bai3/>
+  <HelloWorld msg="31 - Van Minh Thanh Tai" />
 </template>
 
 <style scoped>
